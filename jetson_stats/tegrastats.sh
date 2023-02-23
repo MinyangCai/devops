095164b539af599610a9284b5a9426ff0cfb4a75
MinyangCai <1042553879qq@gmai.com>
2023-02-23 01:44:57 -0800

    123

---
+++ jetson_stats/tegrastats.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DEFAULT_LOG_DIF=/var/log/tegrastats/
+LOG_FILE=tegrastats.log
+
+mkdir -p $DEFAULT_LOG_DIF
+
+tegrastats --interval 1000 | stdbuf -o0 sed -e 's/off/0%@0/g' | ts '%Y-%m-%d %H:%M:%.S' >> $DEFAULT_LOG_DIF/$LOG_FILE
